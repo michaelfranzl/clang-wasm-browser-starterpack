5e8b4786a95445f29e2263f07c034411ac431826
Michael K. Franzl <public.michael@franzl.name>
2021-06-14 08:18:05 +0200

    Simpify make rules

--- flags.mk
+++ flags.mk
@@ -1,15 +1,14 @@
-CC=../../wasi-sdk/build/install/opt/wasi-sdk/bin/clang
-LD=../../wasi-sdk/build/install/opt/wasi-sdk/bin/wasm-ld
+WASI_SDK=/opt/wasi-sdk
+
+CC=$(WASI_SDK)/bin/clang
+LD=$(WASI_SDK)/bin/wasm-ld
 
 # Optional flags which can optimize the WebAssembly binaries in space (size) and time (speed).
 CFLAGS_OPTIMIZATION = \
 	-O3 \
-	-flto \
-	-fwhole-program-vtables \
-	-fvirtual-function-elimination
+	-flto
 
 CFLAGS= \
-	--sysroot ../../wasi-sdk/build/install/opt/wasi-sdk/share/wasi-sysroot \
 	$(CFLAGS_OPTIMIZATION)
 
 LDFLAGS_OPTIMIZATION = \
